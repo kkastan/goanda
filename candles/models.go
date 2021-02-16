bbc24539878e28dc58c5ab0470230a78153ec3fc
Karl Kastan <karlkastan@gmail.com>
2021-02-15 17:34:15 -0800

    document candle models

--- candles/models.go
+++ candles/models.go
@@ -1,11 +1,15 @@
 package candles
 
+// CandleResponse represents the top level Oanda CandlestickResponse model.
+// See https://developer.oanda.com/rest-live-v20/instrument-df/
 type CandleResponse struct {
 	Instrument  string   `json:"instrument"`
 	Granularity string   `json:"granularity"`
 	Candles     []Candle `json:"candles"`
 }
 
+// Candle represents the Oanda Candlestick model.
+// See https://developer.oanda.com/rest-live-v20/instrument-df/
 type Candle struct {
 	Complete bool             `json:"complete"`
 	Volume   int64            `json:"volume"`
@@ -15,6 +19,8 @@ type Candle struct {
 	Mid      *CandleStickData `json:"mid"`
 }
 
+// CandleStickData represents the Oanda CandlestickData model.
+// See https://developer.oanda.com/rest-live-v20/instrument-df/
 type CandleStickData struct {
 	Open  string `json:"o"`
 	High  string `json:"h"`
